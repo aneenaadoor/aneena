b7786370ae9c93ed2b24d40156d11a7169e18196
ABArun95 <arunab5500@gmail.com>
2017-04-26 14:18:42 +0530

    first commit

---
+++ sum.sh
@@ -0,0 +1,26 @@
+s = 0
+echo "enter first number"
+read n1
+echo "enter second number"
+read n2
+s=`expr $n1 + $n2`
+echo "sum is $s"
+echo "enter the first number"
+read n1
+echo "enter second number"
+read n2
+d=`expr $n1 - $n2`
+echo "difference is$d"
+echo "enter first number"
+read n1
+echo "enter the second number"
+read n2
+div=`expr $n1 / $n2`
+echo "division is$div"
+echo "enter the first number"
+read n1
+echo "enter the second number"
+read n2
+m=`expr $n1 \* $n2`
+echo "multiplication of the num is $m"
+
